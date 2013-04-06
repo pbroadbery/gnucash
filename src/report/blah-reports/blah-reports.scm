d3662353156b3da0f33568fc4487461711d7394e
Peter Broadbery <p.broadbery@gmail.com>
2013-04-06 16:57:09 +0100

    NOTE - MOVED TO A NEW BRANCH - pab/topic/report-rewrite/v1

--- src/report/blah-reports/blah-reports.scm
+++
@@ -1,5 +0,0 @@
-(define-module (gnucash report blah-reports))
-(use-modules (srfi srfi-13))
-(use-modules (gnucash main)) ;; FIXME: delete after we finish modularizing.
-(use-modules (gnucash core-utils))
-
